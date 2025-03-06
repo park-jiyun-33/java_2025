f728a63ef0c807db17d520c2e398f75d7c507630
park-jiyun-33 <jiyun7395@naver.com>
2025-03-06 17:34:22 +0900

    Delete java_2025-main/java_01/src directory

--- java_2025-main/java_01/src/com/company/java_part001/SelfTest001.java
+++
@@ -1,7 +0,0 @@
-package com.company.java_part001;
-
-public class SelfTest001 {
-	public static void main(String[] args) {
-		System.out.println("Hello");
-	}
-}
--- java_2025-main/java_01/src/com/company/java_part001/SelfTest009.java
+++
@@ -1,8 +0,0 @@
-package com.company.java_part001;
-
-public class SelfTest009 {
-	public static void main(String[] args) {
-		int i = 1;
-		while(i <= 10) {System.out.print( (i == 1 ? "" : ", ") + i); i++;}
-	}
-}
