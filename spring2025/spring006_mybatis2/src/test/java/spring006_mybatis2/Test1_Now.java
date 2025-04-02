379a28ee159735a1caf8f4853753c2790e4985e4
park-jiyun-33 <jiyun7395@naver.com>
2025-04-02 17:59:57 +0900

    20250402_spring

---
+++ spring2025/spring006_mybatis2/src/test/java/spring006_mybatis2/Test1_Now.java
@@ -0,0 +1,24 @@
+package spring006_mybatis2;
+
+import javax.sql.DataSource;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.company.dao.TestDao;
+
+@RunWith(SpringJUnit4ClassRunner.class)  // 1. spring 구동테스트 
+@ContextConfiguration(locations = "classpath:config/root-context.xml")  // 2. 설정
+public class Test1_Now {
+	@Autowired ApplicationContext context; // 3. Bean(스프링이 관리하는 객체) 생성 ~ 소멸까지
+	@Autowired DataSource datasource;
+	@Autowired TestDao dao;
+	
+	@Ignore public void test0() {System.out.println(context);}
+	@Test public void test1() {System.out.println(datasource);}
+}
